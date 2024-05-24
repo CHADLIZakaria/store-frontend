97ee65e33370c9014c57c238142ef101bccdf701
CHADLIZakaria <zakaria.chadli05@gmail.com>
2024-05-24 20:10:34 +0100

    override apiUrl

--- src/environnments/environnment.ts
+++ src/environnments/environnment.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api/',
-    apiAdminUrl: 'http://localhost:8080/api/admin/'
+    //apiUrl: 'http://localhost:8080/api/',
+    //apiAdminUrl: 'http://localhost:8080/api/admin/',
+    apiUrl: 'https://store-backend-qe1d.onrender.com/api/',
+    apiAdminUrl: 'https://store-backend-qe1d.onrender.com/api/admin/'
 };
